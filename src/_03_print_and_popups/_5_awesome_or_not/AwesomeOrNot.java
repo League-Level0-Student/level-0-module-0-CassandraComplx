a14cf6c26411d1e110e5c3f35709a6483dba2906
league <karinabriellen@gmail.com>
2023-11-19 11:23:46 -0800

    Finished awesome or not

--- src/_03_print_and_popups/_5_awesome_or_not/AwesomeOrNot.java
+++ src/_03_print_and_popups/_5_awesome_or_not/AwesomeOrNot.java
@@ -7,38 +7,42 @@ import javax.swing.JOptionPane;
 public class AwesomeOrNot {
 
 	// 1. Make a main method that includes everything below
-
+public static void main(String[] args) {
 	
-		Random ran = new Random();    //This will be used below to make a random number. 
+
+		 //This will be used below to make a random number. 
 		
 		// 2. Make a variable that will hold a random whole number
-		int count = ran.nextInt(4);
+	Random ran = new Random();	
+	
+	int variable = ran.nextInt(4);
 		// 3. Set your variable equal to a positive number less than 4 using ran.nextInt(4); 
 		
 		// 3. Print your variable to the console
 		
 		// 4. Get the user to enter something that they think is awesome
-		String input = JOptionPane.showInputDialog("Type something you think is awesome");{
+		String input = JOptionPane.showInputDialog("Type something you think is awesome");
 		// 5. If your variable is  0
-		System.out.print(int.class);
-		if (int.class.equals(0)) {
+		System.out.print(variable);
+		if (variable == 0) {
 			JOptionPane.showMessageDialog(null, "Awesome!");
 		} 
-		if (int.class.equals(1)) {
+		if (variable == 1) {
 			JOptionPane.showMessageDialog(null, "Nice");
 		} 
-		if (int.class.equals(2)) {
+		if (variable == 2) {
 			JOptionPane.showMessageDialog(null, "It's ok :/");
 		} 
-		if (int.class.equals(0)) {
+		if (variable == 3) {
 			JOptionPane.showMessageDialog(null, "Boring");
 		} 
 			// -- tell the user whatever they entered is awesome!
-	
+
 		// 6. If your variable is  1
 	
 			// -- tell the user whatever they entered is ok.
-	
+}
+}
 		// 7. If your variable is  2
 	
 			// -- tell the user whatever they entered is boring.
@@ -47,4 +51,4 @@ public class AwesomeOrNot {
 	
 			// -- invent your own message to give to the user (be nice).
 
-}}
+		
